b272dfd8d89c9c6887141e4f7d71b2ae879bec8a
Galih Tama <galpt@v.recipes>
2026-05-26 02:38:36 +0400

    i18n: add Indonesian translation (#16)
    Signed-off-by: Galih Tama <galpt@v.recipes>

---
+++ lang/scx-manager_id.ts
@@ -0,0 +1,89 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="id_ID">
+<context>
+    <name>SchedExtWindow</name>
+    <message>
+        <location filename="../src/schedext-window.ui" line="17"/>
+        <source>CachyOS Configure sched-ext</source>
+        <translation>CachyOS Konfigurasi sched-ext</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="40"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Configure sched-ext scheduler:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;Konfigurasi penjadwal sched-ext:&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="75"/>
+        <source>Running sched-ext scheduler:</source>
+        <translation>Penjadwal sched-ext yang berjalan:</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="82"/>
+        <source>Select sched-ext scheduler:</source>
+        <translation>Pilih penjadwal sched-ext:</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="92"/>
+        <source>Select scheduler profile:</source>
+        <translation>Pilih profil penjadwal:</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="115"/>
+        <source>unknown</source>
+        <translation>tidak diketahui</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="122"/>
+        <source>Set sched-ext extra scheduler flags:</source>
+        <translation>Atur flag tambahan penjadwal sched-ext:</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="176"/>
+        <source>Cancel</source>
+        <translation>Batal</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="183"/>
+        <source>Disable</source>
+        <translation>Nonaktifkan</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window.ui" line="190"/>
+        <source>Apply</source>
+        <translation>Terapkan</translation>
+    </message>
+</context>
+<context>
+    <name>scxctl::impl::SchedExtWindow</name>
+    <message>
+        <location filename="../src/schedext-window-internal.cpp" line="122"/>
+        <source>Cannot initialize scx_loader configuration</source>
+        <translation>Tidak dapat menginisialisasi konfigurasi scx_loader</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window-internal.cpp" line="138"/>
+        <source>Cannot get information from scx_loader!
+Is it working?
+This is needed for the app to work properly</source>
+        <translation>Tidak dapat memperoleh informasi dari scx_loader!
+Apakah scx_loader berfungsi?
+Ini diperlukan agar aplikasi dapat berjalan dengan baik</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window-internal.cpp" line="206"/>
+        <source>Cannot disable scx_loader</source>
+        <translation>Tidak dapat menonaktifkan scx_loader</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window-internal.cpp" line="224"/>
+        <source>Cannot get scx flags from scx_loader configuration!</source>
+        <translation>Tidak dapat memperoleh flag scx dari konfigurasi scx_loader!</translation>
+    </message>
+    <message>
+        <location filename="../src/schedext-window-internal.cpp" line="260"/>
+        <source>Cannot set default scx scheduler with mode! Scheduler %1 with mode %2</source>
+        <translation>Tidak dapat mengatur penjadwal scx bawaan dengan mode! Penjadwal %1 dengan mode %2</translation>
+    </message>
+</context>
+</TS>
